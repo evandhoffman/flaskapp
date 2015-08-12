d00603a4919a7e1972a0d8e641d95c5ce78206db
Evan D. Hoffman <evandhoffman@gmail.com>
2015-08-12 18:13:07 -0400

    recipes

--- chef/flaskapp/recipes/default.rb
+++ chef/flaskapp/recipes/default.rb
@@ -6,3 +6,27 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'poise-python'
+
+# Configure nginx
+
+template "/etc/nginx/sites-available/blog.conf" do
+  source "nginx-blog.conf.erb"
+  owner "root"
+  group "root"
+  mode "0744"
+end
+
+# deploy code & start gunicorn
+
+application '/vital/devops_blog' do
+  git 'https://github.com/vitalco/devops_blog'
+  pip_requirements
+  gunicorn do
+    port 5000
+  end
+end
+
+
+  
