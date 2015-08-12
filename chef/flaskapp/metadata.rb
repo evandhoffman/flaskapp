1a2f3dd292a1b7ec2c6eadfd1c1813e8e73411fb
Evan D. Hoffman <evandhoffman@gmail.com>
2015-08-12 18:17:12 -0400

    added dependency

--- chef/flaskapp/metadata.rb
+++ chef/flaskapp/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures flaskapp'
 long_description 'Installs/Configures flaskapp'
 version          '0.1.0'
+
+depends           'application_python'
